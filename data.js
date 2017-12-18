a0c566caea3489abca1b28e9228fff75d84ced2c
Nikolay Atamanyuk <ezhi.mail@gmail.com>
2017-12-18 18:39:24 +0300

    Go to knockout base

---
+++ data.js
@@ -0,0 +1,310 @@
+﻿
+var data={
+	sections:[
+	{
+		name:"Разное",
+		anchor:"stuff",
+		conserts:[
+		{
+			name:"Обыкновенное чудо",
+			date:"2005-03-19",
+			stage:"Дом Офицеров",
+			owner:"СБ",
+			videoLink:"https://mega.nz/#F!QiYm2JwD!9Uy5uN5ko4tmOI64MKYagQ",
+			actors:[],
+			compositions:[]
+		},
+		{
+			name:"И это все о нас",
+			date:"2007-03-31",
+			stage:"Дом Офицеров",
+			owner:"СБ",
+			videoLink:"https://mega.nz/#F!M3wExZ7S!aaNwO9-JSRp3XDtrAF2Deg",
+			actors:[],
+			compositions:[]
+		},
+		{
+			name:"Кубок Кремля - Ирландия",
+			date:"2007-10-14",
+			stage:"Спорткомплекс Мещерский",
+			owner:"Бальные танцы",
+			videoLink:"https://mega.nz/#F!xjBjABjJ!RblPFWi3K0HAU9V71EEKXQ",
+			actors:[],
+			compositions:[]
+		},
+		{
+			name:"День города с филармонией",
+			date:"2008-09-13",
+			stage:"Площадь Минина и Пожарского",
+			owner:"Город",
+			videoLink:"https://mega.nz/#F!Vvp1kKhb!57kO2OMlzjOwkB15jZy15A",
+			actors:[],
+			compositions:["Луна", "Лядов - Куклы", "Петров - Петербуржские тайны", "Хачатурян - Ирландия"]
+		},
+		{
+			name:"Маленькие истории больших городов",
+			date:"2012-05-26",
+			stage:"СДК",
+			owner:"СБ",
+			videoLink:"https://mega.nz/#F!cvZCRQhD!dRNGKR2KcsVEVrZdRvmBzQ",
+			actors:[],
+			compositions:[]
+		},
+		{
+			name:"Репетиция столица Нижний",
+			date:"2012-12-12",
+			stage:"Панина",
+			owner:"СБ",
+			videoLink:"https://mega.nz/#F!U3pyxSxT!g_MvG2Q1mHkgAtt5q3Skcg",
+			actors:[],
+			compositions:[]
+		},
+		]
+	},
+	{
+		name:"Карнавальсы",
+		anchor:"valts",
+		conserts:[
+		{
+			name:"Карнавальс",
+			date:"2008-01-13",
+			stage:"ККЗ",
+			owner:"Филармония",
+			videoLink:"https://mega.nz/#F!NqJ2lY4L!bUlUun3I2pPDP9y_1Z8lFA",
+			actors:[],
+			compositions:["КТ - Военный", "КТ - Коньки", "КТ - Фонарики", "Лядов - Куклы", "Петров - Петербуржские тайны", "Свиридов - Сказки", "Хачатурян - Ирландия", "После спектакля"]
+		},
+		{
+			name:"Карнавальс",
+			date:"2009-01-13",
+			stage:"ККЗ",
+			owner:"Филармония",
+			videoLink:"https://mega.nz/#F!5rAixQJJ!N5EVd0snsagXhpQ7eMnDKQ",
+			actors:[],
+			compositions:["Гаврилин - Анюта", "Дунаевский - Школьный вальс", "Ребиков - Елка", "Рубинштейн - Испанский", "Таривердиев - Ирония судьбы", "Чайковский - Подснежник", "_", "_"]
+		},
+		{
+			name:"Карнавальс",
+			date:"2010-01-13",
+			stage:"ККЗ",
+			owner:"Филармония",
+			videoLink:"https://mega.nz/#F!ZvgnCZZb!oGRwyCZkm1Y37RZpYDsNgA",
+			actors:[],
+			compositions:["Вальс - А зори здесь тихие", "Глазунов - Золотой степ", "Петров - Зонтики", "Три вальса - Лена", "Чайковский - Святки", "Шарманка", "_", "_"]
+		},
+		{
+			name:"Карнавальс",
+			date:"2011-01-13",
+			stage:"ККЗ",
+			owner:"Филармония",
+			videoLink:"https://mega.nz/#F!R6QyFCZK!QbmPkYFpxcV1KX448LBkHA",
+			actors:[],
+			compositions:["Дога - Мой ласковый и нежный зверь", "Кармен-сюита - Лена", "Лядов - Куклы", "Петров - Зонтики", "Петров - Петербуржские тайны", "Таривердиев - Ирония судьбы", "Хачатурян - Ирландия", "_", "_"]
+		},
+		{
+			name:"Карнавальс",
+			date:"2012-01-13",
+			stage:"ККЗ",
+			owner:"Филармония",
+			videoLink:"https://mega.nz/#F!NmYSwD6B!R7NodF50PlIgJhoS5S2psA",
+			actors:[],
+			compositions:["Глинка - Арагонская хота", "Дашкевич - Пляшущие человечки", "Дашкевич - Цирк", "Дога - Мой ласковый и нежный зверь", "Таривердиев - Ирония судьбы", "Теодоракис - Сиртаки", "Хачатурян - Лезгинка", "Хачатурян - Триумф", "_"]			
+		},
+		{
+			name:"Карнавальс",
+			date:"2013-01-13",
+			stage:"ККЗ",
+			owner:"Филармония",
+			videoLink:"https://mega.nz/#F!s7AEEZKY!kxIpaziKhoQCCU_iPxwZiw",
+			actors:[],
+			compositions:["Бородин - Половецкие пляски", "Глазунов - Золотой степ", "Оффенбах - Орфей в аду", "Петров - Зонтики", "Родригес - Воришки", "Соловьев-Седой - Пляска парубков и девчат", "Хачатурян - Флаги", "Штраус - Полька Анна"]			
+		},
+		{
+			name:"Карнавальс",
+			date:"2014-01-13",
+			stage:"ККЗ",
+			owner:"Филармония",
+			videoLink:"https://mega.nz/#F!o2JzmKCb!seJOUnJyCI0VfgA-JnESRw",
+			actors:[],
+			compositions:["Бородин - Половецкие пляски", "Оффенбах - Орфей в аду", "Петров - Подводный мир", "Прокофьев - Шоколадница", "Римский-Корсаков - Шехеризада", "Свиридов - Сказки", "Фомин - Пляска фурий", "Хачатурян - Ирландия"]						
+		},
+		{
+			name:"Карнавальс",
+			date:"2015-01-13",
+			stage:"ККЗ",
+			owner:"Филармония",
+			videoLink:"https://mega.nz/#F!xjxGTYKL!tUb6gNN03O8W9b1eL_sbyA",
+			actors:[],
+			compositions:["Глазунов - Большой вальс", "Григ - Снежная королева", "Морриконе - Письма", "Петров - Вася", "Соловьев-Седой - Пляска парубков и девчат", "Хачатурян - Вакханалия", "Чайковский - Подснежник"]						
+		},
+		{
+			name:"Карнавальс у Рукавишниковых",
+			date:"2016-01-13",
+			stage:"ККЗ",
+			owner:"Филармония",
+			videoLink:"https://mega.nz/#F!9nBWULqK!8e8hF1LzA00z39fNWV0VvA",
+			actors:[],
+			compositions:["Таривердиев - Ирония судьбы", "Хачатурян - Эгина", "Чайковский - Святки", "Штраус - Полька Анна", "_", "_", "_", "_"]						
+		},
+		]
+	},
+	{
+		name:"Троя",
+		anchor:"troy",
+		conserts:[
+		{
+			name:"Троя",
+			date:"2010-05-16",
+			stage:"СДК",
+			owner:"СБ",
+			videoLink:"https://mega.nz/#F!omZWRYJB!A4npzVKu57D2bK6XHK7OqA",
+			actors:[
+			{ role:"Гера", actor:"Алена Буркова"},
+			{ role:"Афина", actor:"Елена Еркулова"},
+			{ role:"Менелай/Приам", actor:"Александр Демидов"},
+			{ role:"Касандра", actor:"Екатерина Кукушкина"},
+			{ role:"Гектор", actor:"Юлия Круглова"},
+			{ role:"Патрокл", actor:"Владислав Торников"},
+			{ role:"Ахилес", actor:"Елена Хиценко"},			
+			],
+			compositions:[]
+		},
+		{
+			name:"Троя",
+			date:"2010-11-10",
+			stage:"ДК Газ",
+			owner:"СБ",
+			videoLink:"https://mega.nz/#F!w353Ea7A!g_Iv1l13FRimvsmpHm6tlA",
+			actors:[
+			{ role:"Гера", actor:"Алена Буркова"},
+			{ role:"Афина", actor:"Елена Еркулова"},
+			{ role:"Менелай/Приам", actor:"Александр Демидов"},
+			{ role:"Касандра", actor:"Ксения Урванцева"},
+			{ role:"Гектор", actor:"Юлия Круглова"},
+			{ role:"Патрокл", actor:"Владислав Торников"},
+			{ role:"Ахилес", actor:"Елена Хиценко"},			
+			],
+			compositions:[]
+		}
+		]
+	},
+	{
+		name:"Вечера на хуторе близ Диканьки",
+		anchor:"dicanka",
+		conserts:[
+		{
+			name:"Вечера на хуторе близ Диканьки",
+			date:"2012-05-26",
+			stage:"СДК",
+			owner:"СБ",
+			videoLink:"https://mega.nz/#F!pjA3nJgJ!AyiIWZTpifI01vovflkR4Q",
+			actors:[
+			{ role:"Оксана", actor:"Юлия Еробкина"},
+			{ role:"Галя", actor:"Ксения Урванцева"},
+			],
+			compositions:[]
+		}
+		]
+	},
+	{
+		name:"Золушка",
+		anchor:"goldy",
+		conserts:[
+		{
+			name:"Золушка",
+			date:"2016-10-08",
+			stage:"СДК",
+			owner:"СБ",
+			videoLink:"https://mega.nz/#F!8nY1HIZK!y3NpFAKr6e8q0RybVlUTWA",
+			actors:[
+			{ role:"Золушка", actor:"Изабелла Спехова"},
+			{ role:"Кубышка", actor:"Валерия Киселева"},
+			{ role:"Худышка", actor:"Анастасия Коробова"},
+			{ role:"Фея крестная", actor:"Бэлла Мухарметова"},
+			{ role:"Танцмейстер", actor:"Дарья Донина"},
+			],
+			compositions:[]
+		},
+		{
+			name:"Золушка",
+			date:"2016-10-15",
+			stage:"СДК",
+			owner:"СБ",
+			videoLink:"https://mega.nz/#F!diwRjLST!zSAQqnTY6LNFQnRJAqFZzg",
+			actors:[
+			{ role:"Золушка", actor:"Изабелла Спехова"},
+			{ role:"Кубышка", actor:"Валерия Киселева"},
+			{ role:"Худышка", actor:"Анастасия Коробова"},
+			{ role:"Фея крестная", actor:"Бэлла Мухарметова"},
+			{ role:"Танцмейстер", actor:"Дарья Донина"},
+			],
+			compositions:[]
+		},
+		{
+			name:"Золушка",
+			date:"2016-10-29",
+			stage:"ДК Газ",
+			owner:"СБ",
+			videoLink:"https://mega.nz/#F!kvo0gR7S!-_xizc22DI3OZOvuZp-niA",
+			actors:[
+			{ role:"Золушка", actor:"Изабелла Спехова"},
+			{ role:"Кубышка", actor:"Анна Прокопенко"},
+			{ role:"Худышка", actor:"Анастасия Коробова"},
+			{ role:"Фея крестная", actor:"Варвара Парамонова"},
+			{ role:"Танцмейстер", actor:"Ольга Шиндина"},
+			],
+			compositions:[]
+		},
+		{
+			name:"Золушка",
+			date:"2016-11-05",
+			stage:"СДК",
+			owner:"СБ",
+			videoLink:"https://mega.nz/#F!QuhRFTBY!mmfRq3rCd7aa1dWoGYyFlA",
+			actors:[
+			{ role:"Золушка", actor:"Ирина Шелешнева"},
+			{ role:"Кубышка", actor:"Валерия Киселева"},
+			{ role:"Худышка", actor:"Екатерина Цюпий"},
+			{ role:"Фея крестная", actor:"Бэлла Мухарметова"},
+			{ role:"Танцмейстер", actor:"Дарья Донина"},
+			],
+			compositions:[]
+		}
+		]
+	},
+	{
+		name:"Камерный Театр",
+		anchor:"kt",
+		conserts:[
+		{
+			name:"8 марта",
+			date:"2017-03-08",
+			stage:"СДК",
+			owner:"КТ",
+			videoLink:"https://mega.nz/#F!B3pQSZoZ!gjWldK16eYTvZuUpZkvq6Q",
+			actors:[],
+			compositions:[]
+		}
+		]
+	},
+	/*
+	{
+		name:"empty",
+		anchor:"empty",
+		conserts:[
+		{
+			name:"con",
+			date:"date",
+			stage:"stage",
+			owner:"owner",
+			videoLink:"link",
+			actors:[
+			{ role:"role", actor:"actor" }
+			],
+			compositions:["comp1"]
+		}
+		]
+	},
+	*/
+	]
+};
---
+++ main.js
@@ -0,0 +1,8 @@
+﻿
+(function(data){
+
+	var self=this;
+	var vm=ko.mapping.fromJS(data);
+	
+	ko.applyBindings(vm, document.getElementById("sectionsList"))
+})(data);
